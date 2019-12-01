11a61586433252cf72a5d3c689441a9190cef549
Sean Huang <hyprul@ucla.edu>
2019-12-01 16:54:49 -0600

    entry practice

---
+++ mapping-components-practice/src/components/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import emojipedia from '../emojipedia';
+import Entry from './Entry';
+
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">
+
+        {emojipedia.map(entry => (
+          <Entry
+            key={entry.id}
+            emoji={entry.emoji}
+            name={entry.name}
+            meaning={entry.meaning}
+          />
+        ))}
+
+      </dl>
+    </div>
+  );
+}
+
+export default App;
